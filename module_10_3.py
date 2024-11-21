dedbf44ddf71d843a90fee53d9f8ea0be7f43bf0
RystikS <rustamsagadeev@gmail.com>
2024-11-21 14:21:53 +0500

    Update module_10_3.py

--- module_10_3.py
+++ module_10_3.py
@@ -2,34 +2,37 @@ import threading
 import time
 import random
 
-class Bank(threading.Thread):
+
+class Bank:
     def __init__(self, balance: int):
         self.balance = balance
         self.lock = threading.Lock()
         self.delay = 0.001
 
     def deposit(self):
-
         for i in range(100):
             random_num = random.randint(50, 500)
             with self.lock:
                 self.balance += random_num
-                print(f'Пополнение:{random_num}. Баланс: {self.balance}')
+                print(f'Пополнение:{random_num}. Баланс: {self.balance}', f'Порядок итерации {i}')
+            if self.balance >= 500 and self.lock.locked():
+                self.lock.release()
+
+
             time.sleep(self.delay)
 
     def take(self):
         for i in range(100):
-            self.lock.acquire()
             random_num = random.randint(50, 500)
-            print(f'Запрос на {random_num}')
-            try:
-                if random_num <= self.balance:
-                    self.balance -= random_num
-                    print(f'Снятие: {random_num}. Баланс: {self.balance}')
-                else:
+            with self.lock:
+                print(f'Запрос на {random_num}', f'Порядок итерации {i}')
+                if random_num > self.balance:
                     print(f'Запрос отклонен, недостаточно средств')
-            finally:
-                lock.release()
+                    self.lock.acquire()
+                else:
+                    self.balance -= random_num
+                    print(f'Снятие: {random_num}. Баланс: {self.balance}', f'Порядок итерации {i}')
+
             time.sleep(self.delay)
 
 
